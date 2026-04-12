8e4bf06bcd6fb8fb0add1d77825ac227b93c8346
shinsurim <surimshin@gmail.com>
2026-04-12 21:23:24 +0900

    transfer test

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -3,11 +3,14 @@ import { expect } from "chai";
 import { MyToken } from "../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
-describe("mytoken deploy", () => {
+const mintingAmount = 100n;
+const decimals = 18n;
+
+describe("My Token", () => {
     let myTokenC: MyToken;
     let signers: HardhatEthersSigner[];
-  before("should deploy", async () => {
-    //before 이 프로그램을 시작하기 전에 시작해줘
+  beforeEach("should deploy", async () => {
+    //before 이 프로그램을 시작하기 전에 그룹별로 각각 시작해줘
     signers = await hre.ethers.getSigners();
 
     //hardhat은 기본적으로 signer0 사용
@@ -16,34 +19,55 @@ describe("mytoken deploy", () => {
       "MyToken",
       "MT",
       18,
+      100
     ]);
   });
+
+  describe("Basic state value check", () => {
     it("should return", async () => {
-    expect(await myTokenC.name()).equal("MyToken");
-    });
+        expect(await myTokenC.name()).equal("MyToken");
+        });
 
-    it("should return", async () => {
-    expect(await myTokenC.symbol()).equal("MT");
-    });
+        it("should return", async () => {
+        expect(await myTokenC.symbol()).equal("MT");
+        });
 
-    it("should return", async () => {
-    expect(await myTokenC.decimals()).equal(18);
-    });
+        it("should return", async () => {
+        expect(await myTokenC.decimals()).equal(18);
+        });
 
-    it("should return 0 totalSupply", async () => {
-    expect(await myTokenC.totalSupply()).equal(1n*10n**18n);
+        it("should return 100 totalSupply", async () => {
+        expect(await myTokenC.totalSupply()).equal(mintingAmount*10n**decimals);
     });
-
-    //1MT =1*10^18
+  }) 
+    
+  //1MT =1*10^18
+  describe("Mint", ()=> {
     it("should return 1MT balance for signer 0", async () => {
-    const signer0 = signers[0];
-    expect(await myTokenC.balanceOf(signer0)).equal(1n*10n**18n);
-  });
-  //state을 읽기만 하는 함수와 건드리는 함수 어떻게 처리가 되는지 정확하게 알아야함
-  it("should have 0.5MT", async () => {
-    const signer1 = signers[1];
-    await myTokenC.transfer(hre.ethers.parseUnits("0.5", 18), signer1.address);
-    expect(await myTokenC.balanceOf(signer1.address)).equal(hre.ethers.parseUnits("0.5", 18));
-  });
+      const signer0 = signers[0];
+      expect(await myTokenC.balanceOf(signer0)).equal(mintingAmount*10n**decimals);
+    });
+  })
+  
 
+  //state을 읽기만 하는 함수와 건드리는 함수 어떻게 처리가 되는지 정확하게 알아야함
+  describe("Transfer", () => {
+      it("should have 0.5MT", async () => {
+      const signer1 = signers[1];
+      await myTokenC.transfer(hre.ethers.parseUnits("0.5", decimals), signer1.address);
+      expect(await myTokenC.balanceOf(signer1.address)).equal(hre.ethers.parseUnits("0.5", decimals));
+    });
+
+    it("should be reverted with insufficient balance error", async () => {
+      const signer1 = signers[1];
+      //await 위치 중요 -> expect를 테스트하는 특수한 상황
+      await expect( // 따라 오는 코드를 줘서 모카 프래임워크한테 잘 실행되는지 확인해줘라고 하는 것
+          myTokenC.transfer(
+          hre.ethers.parseUnits((mintingAmount+1n).toString(), decimals),
+          signer1.address
+        )
+      ).to.be.revertedWith("insufficient balance");
+    });
+  })
+  
 });
